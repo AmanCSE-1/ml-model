0fb46e3920a5180dc96a9347a22770a92c523b87
AmanCSE-1 <78953880+AmanCSE-1@users.noreply.github.com>
2023-04-20 20:10:42 +0530

    Add `chart_visualization` function to return data to plot graph

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 import uvicorn
 from pydantic import BaseModel
 
+import pandas as pd
 import joblib
 
 
@@ -40,6 +41,42 @@ def root():
     return {'status': 'SUCCESS',
             'message': 'This is a root directory'}
 
+# Chart visualization function that returns pie chart data for features 
+def chart_visualization(industry):
+    url = 'https://drive.google.com/file/d/1I4ZFa3weBOozdAitMOHVvopOVvbyzvHm/view?usp=sharing'
+    url = 'https://drive.google.com/uc?id=' + url.split('/')[-2]
+
+    df = pd.read_csv(url)
+
+    industry_mapping = {0: 'Comedy', 1: 'Direct', 2: 'Drama', 3: 'Infomercial', 4: 'Other'}
+    industry = industry_mapping[industry]
+    df = df[(df['industry'] == industry) & (df['click'] == 1)]
+
+    # advertisement feature
+    banner_pos_graph_data = df['banner_pos'].value_counts().to_dict()
+    banner_pos_graph_data = [{'name': k, 'value': v} for k, v in banner_pos_graph_data.items()]
+
+    site_domain_graph_data = df['site_domain'].value_counts().to_dict()
+    site_domain_graph_data = [{'name': k, 'value': v} for k, v in site_domain_graph_data.items()]
+
+    genre_graph_data = df['genre'].value_counts().to_dict()
+    genre_graph_data = [{'name': k, 'value': v} for k, v in genre_graph_data.items()]
+
+    # user feature
+    targeted_sex_graph_data = df['targeted_sex'].value_counts().to_dict()
+    targeted_sex_graph_data = [{'name': k, 'value': v} for k, v in targeted_sex_graph_data.items()]
+
+    relationship_status_graph_data = df['realtionship_status'].value_counts().to_dict()
+    relationship_status_graph_data = [{'name': k, 'value': v} for k, v in relationship_status_graph_data.items()]
+
+    graph_data_array = [{'banner_pos': banner_pos_graph_data,
+                         'site_domain': site_domain_graph_data,
+                         'genre': genre_graph_data,
+                         'targeted_sex': targeted_sex_graph_data,
+                         'relationship_status': relationship_status_graph_data}]
+
+    return graph_data_array
+
 
 # Creating an Endpoint to receive the data to make prediction on.
 @app.post("/predict/")
@@ -61,8 +98,12 @@ def predict(input_data: Item):
     prediction_result = model.predict_proba([[banner_pos, site_domain, device_type, relationship_status, industry, genre,
                                         targeted_sex, airtime, airlocation, expensive, money_back_guarantee,
                                         hour_of_day]])
-
+    
+    # Call the graph visualization function
+    graph_data_array = chart_visualization(industry)
+    
     # Return the Result
     return {'status': 'SUCCESS',
             'message': 'Prediction is made successfully',
-            'CTR_result': str(prediction_result[0][1])}
+            'CTR_result': str(prediction_result[0][1]),
+            'graph_visualization': graph_data_array}
