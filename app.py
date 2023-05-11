4e7560e03f543c1aafe07755172feb8cd62452d4
AmanCSE-1 <78953880+AmanCSE-1@users.noreply.github.com>
2023-05-11 13:12:16 +0530

    Update `chart_visualization` method
    Resolved the latency for loading file through Google Drive

--- app.py
+++ app.py
@@ -43,37 +43,44 @@ def root():
 
 # Chart visualization function that returns pie chart data for features 
 def chart_visualization(industry):
-    url = 'https://drive.google.com/file/d/1I4ZFa3weBOozdAitMOHVvopOVvbyzvHm/view?usp=sharing'
-    url = 'https://drive.google.com/uc?id=' + url.split('/')[-2]
-
-    df = pd.read_csv(url)
-
     industry_mapping = {0: 'Auto', 1: 'ClassAction', 2: 'Entertainment', 3: 'Other', 4: 'Pharma', 5: 'Political'}
     industry = industry_mapping[industry]
-    df = df[(df['industry'] == industry) & (df['click'] == 1)]
 
-    # advertisement feature
-    banner_pos_graph_data = df['banner_pos'].value_counts().to_dict()
-    banner_pos_graph_data = [{'name': k, 'value': v} for k, v in banner_pos_graph_data.items()]
+    site_domain_entire_data = {'Auto': {'Facebook': 20106,'Instagram': 14439,'Twitter': 9354,'LinkedIn': 6498,'Snapchat': 2923},
+                               'ClassAction': {'Facebook': 1207,'Instagram': 880,'Twitter': 558,'LinkedIn': 416,'Snapchat': 197},
+                               'Entertainment': {'Facebook': 5693,'Instagram': 3984,'Twitter': 2559,'LinkedIn': 1772,'Snapchat': 848},
+                               'Other': {'Facebook': 18106,'Instagram': 13104,'Twitter': 8289,'LinkedIn': 5786,'Snapchat': 2635},
+                               'Pharma': {'Facebook': 135785,'Instagram': 98051,'Twitter': 62546,'LinkedIn': 43351,'Snapchat': 19900},
+                               'Political': {'Facebook': 2730,'Instagram': 2070,'Twitter': 1277,'LinkedIn': 913,'Snapchat': 414}}
 
-    site_domain_graph_data = df['site_domain'].value_counts().to_dict()
+    site_domain_graph_data = site_domain_entire_data[industry]
     site_domain_graph_data = [{'name': k, 'value': v} for k, v in site_domain_graph_data.items()]
 
-    genre_graph_data = df['genre'].value_counts().to_dict()
+
+    genre_entire_data = {'Auto': {'Comedy': 47299,'Infomercial': 3840,'Drama': 1540,'Other': 434,'Direct': 207},
+                         'ClassAction': {'Comedy': 2210, 'Drama': 420, 'Infomercial': 412, 'Other': 216},
+                         'Entertainment': {'Comedy': 11515, 'Infomercial': 2396, 'Drama': 731, 'Other': 214},
+                         'Other': {'Comedy': 41084,'Infomercial': 3389,'Drama': 2301,'Other': 843,'Direct': 303},
+                         'Pharma': {'Comedy': 332915,'Infomercial': 13316,'Drama': 10271,'Direct': 2280,'Other': 851},
+                         'Political': {'Comedy': 5929, 'Drama': 753, 'Infomercial': 618, 'Other': 104}}
+
+    genre_graph_data = genre_entire_data[industry]
     genre_graph_data = [{'name': k, 'value': v} for k, v in genre_graph_data.items()]
 
     # user feature
-    targeted_sex_graph_data = df['targeted_sex'].value_counts().to_dict()
+    targeted_sex_entire_data = {'Auto': {'Male': 31387, 'Female': 21933},
+                         'ClassAction': {'Male': 2102, 'Female': 1156},
+                         'Entertainment': {'Female': 9305, 'Male': 5551},
+                         'Other': {'Female': 47920, 'Male':18076},
+                         'Pharma': {'Male': 359633, 'Female': 67435},
+                         'Political': {'Male': 4397, 'Female': 3007}}
+
+    targeted_sex_graph_data = targeted_sex_entire_data[industry]
     targeted_sex_graph_data = [{'name': k, 'value': v} for k, v in targeted_sex_graph_data.items()]
 
-    relationship_status_graph_data = df['realtionship_status'].value_counts().to_dict()
-    relationship_status_graph_data = [{'name': k, 'value': v} for k, v in relationship_status_graph_data.items()]
-
-    graph_data_array = [{'banner_pos': banner_pos_graph_data,
-                         'site_domain': site_domain_graph_data,
+    graph_data_array = [{'site_domain': site_domain_graph_data,
                          'genre': genre_graph_data,
-                         'targeted_sex': targeted_sex_graph_data,
-                         'relationship_status': relationship_status_graph_data}]
+                         'targeted_sex': targeted_sex_graph_data}]
 
     return graph_data_array
 
